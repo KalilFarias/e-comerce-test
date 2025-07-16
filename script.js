781adbaa39a2fe51b27b83b42d6ca00afeebebf7
kalilfariax <kalilsandes42@gmail.com>
2025-07-16 10:31:22 -0300

    add images and script off carrousel

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ fetch("templates/navbar.html")
   .then((data) => {
     document.getElementById("header").innerHTML = data;
   });
+
+$(document).ready(function () {
+  $(".featured-carousel").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
