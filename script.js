e0721045261bd16423025bac6be0338484c88323
kalilfariax <kalilsandes42@gmail.com>
2025-07-16 13:54:23 -0300

    add carrousel e cdn de jquery pra rodar

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
-fetch("templates/navbar.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("header").innerHTML = data;
-  });
-
 $(document).ready(function () {
-  $(".featured-carousel").slick({
+  $(".carousel-products").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    dots: true,
     arrows: true,
+    dots: false,
     responsive: [
       {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        },
+        breakpoint: 992,
+        settings: { slidesToShow: 3 },
       },
       {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-        },
+        breakpoint: 768,
+        settings: { slidesToShow: 2 },
+      },
+      {
+        breakpoint: 576,
+        settings: { slidesToShow: 1 },
       },
     ],
   });
