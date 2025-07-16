09cef48a48e4fe7ca88d23ac16db84b202bff8f2
KalilFarias <kalilsandes42@gmail.com>
2025-07-16 15:45:06 -0300

    add dos botoes do carousel e das imagens restantes

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
     autoplay: true,
     autoplaySpeed: 2000,
     arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><</button>',
+    nextArrow: '<button type="button" class="slick-next">></button>',
     dots: false,
     responsive: [
       {
